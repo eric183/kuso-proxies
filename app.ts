26d9b6b3d1d3ff2ca7e159cec11adfd21e40a8d1
eric183 <kk297466058@gmail.com>
2022-08-14 17:05:57 +0800

    updated

---
+++ app.ts
@@ -0,0 +1,36 @@
+export {};
+import * as dotenv from 'dotenv'
+dotenv.config();
+
+import express from "express";
+import axios from "axios";
+import cors from 'cors';
+
+const requestFromApi = async (url: string) => await axios.get(url);
+
+
+const app = express();
+
+
+var corsOptions = {
+  origin: [
+    process.env.corsOrigin!
+  ],
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
+
+app.get('/http*', async (req, res) => {
+    
+  const url = /\/(http.+)/gim.exec(req.url);
+  const proxyUrl = url ? url[1] : req.url;
+
+  const { data } =  await requestFromApi(proxyUrl);
+
+  console.log('url: ', req.url);
+
+  res.send(data);
+
+}).listen(process.env.LISTEN_PORT);
